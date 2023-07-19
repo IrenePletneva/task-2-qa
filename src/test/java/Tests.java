3a7ce55a36edae12ff80b6252c6912a0db2d7a65
IrenePletneva <73946301+IrenePletneva@users.noreply.github.com>
2023-07-19 15:24:21 +0300

    added improvements from comment

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -5,12 +5,13 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class Tests {
     @BeforeAll
     static void beforeAll() {
-        Configuration.startMaximized = true;
+        Configuration.browserSize = "1920x1080";
     }
 
     @Test
@@ -20,47 +21,51 @@ public class Tests {
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('footer').remove()");
 
-        $("[id = firstName]").setValue("Testname");
-        $("[id = lastName]").setValue("TestLastName");
-        $("[id = userEmail]").setValue("qaguru@test.ru");
+        $("#firstName").setValue("Testname");
+        $("#lastName").setValue("TestLastName");
+        $("#userEmail").setValue("qaguru@test.ru");
 
         //Radiobutton
         $("[type='radio'][value='Male']").parent().click();
 
-        $("[id = userNumber]").setValue("1111111111");
+        $("#userNumber").setValue("1111111111");
         //Calendar
-        $("[id = dateOfBirthInput]").click();
+        $("#dateOfBirthInput").click();
 //        $("[id = dateOfBirthInput]").setValue("01 Dec 2022").pressEnter();
-        $("[class=\"react-datepicker__year-select\"]").$("[value=\"2000\"]").click();
-        $("[class=\"react-datepicker__month-select\"]").$("[value=\"7\"]").click();
+        $(".react-datepicker__year-select").$("[value=\"2000\"]").click();
+        $(".react-datepicker__month-select").$("[value=\"7\"]").click();
         $("[class=\"react-datepicker__day react-datepicker__day--011\"]").click();
 
         //Subject
-        $("[id = subjectsInput]").setValue("Physics").pressEnter();
+        $("#subjectsInput").setValue("Physics").pressEnter();
 
-        $("[for=\"hobbies-checkbox-1\"]").click();
-        $("[for=\"hobbies-checkbox-2\"]").click();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+//        $("[for=\"hobbies-checkbox-1\"]").click();
+//        $("[for=\"hobbies-checkbox-2\"]").click();
 
         //file
-        $("[id = uploadPicture]").uploadFile(new File("src/test/resources/img.jpg"));
+        $("#uploadPicture").uploadFromClasspath("img.jpg");
         //Address
-        $("[id = currentAddress]").setValue("Test Address");
+        $("#currentAddress").setValue("Test Address");
 
-        $("[id = react-select-3-input]").setValue("NCR").pressEnter();
-        $("[id = react-select-4-input]").setValue("Delhi").pressEnter();
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
 
-        $("[id = submit]").click();
+
+        $("#submit").click();
 
         //AfterClick Submit
-        $("[id = example-modal-sizes-title-lg]").shouldHave(text("Thanks for submitting the form"));
-        $("[class=\"table table-dark table-striped table-bordered table-hover\"]")
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive")
                 .$("tbody").shouldHave(text("Testname TestLastName"),
                 text("qaguru@test.ru"),
                 text("Male"),
                 text("1111111111"),
                 text("11 August,2000"),
                 text("Physics"),
-                text("Sports, Reading"),
+                text("Sports"),
                 text("img.jpg"),
                 text("Test Address"),
                 text("NCR Delhi"));
